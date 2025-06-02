4633b92af873b0510e98fcd8b3e6805f88d59ee8
baqumz <p.villanueva.fuentes@gmail.com>
2025-06-01 20:34:48 -0400

    mod1

---
+++ shopverse-api/src/main/java/com/technova/shopverseapi/ShopverseApiApplication.java
@@ -0,0 +1,12 @@
+package com.technova.shopverseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopverseApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopverseApiApplication.class, args);
+    }
+}
